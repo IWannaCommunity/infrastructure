ad590970e7302ee3e29c343c16c47c52c083db8c
Starz0r <starz0r@starz0r.com>
2019-07-13 16:22:31 -0500

    Organization ID required for some Cloudflare API routes

--- provider.tf
+++ provider.tf
@@ -8,8 +8,10 @@ provider "digitalocean" {
 # Cloudflare
 variable "cf_apikey" {}
 variable "cf_email" {}
+variable "cf_orgid" {}
 
 provider "cloudflare" {
-  email = "${var.cf_email}"
-  token = "${var.cf_apikey}"
+  email  = "${var.cf_email}"
+  token  = "${var.cf_apikey}"
+  org_id = "${var.cf_orgid}"
 }

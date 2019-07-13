18f6093dd5d9bbb7809e29c3bf5b1be2a5a7cc5f
Starz0r <starz0r@starz0r.com>
2019-07-13 17:38:16 -0500

    Array accessors and getting the proper zone name

--- networking.tf
+++ networking.tf
@@ -3,9 +3,9 @@ resource "cloudflare_zone" "starz0r" {
 }
 
 resource "cloudflare_record" "quassel" {
-  domain = "${cloudflare_zone.starz0r}"
+  domain = "${cloudflare_zone.starz0r.zone}"
   name   = "quassel"
-  value  = "${kubernetes_service.quassel.load_balancer_ingress.ip}"
+  value  = "${kubernetes_service.quassel.load_balancer_ingress.0.ip}"
   type   = "A"
   ttl    = 3600
 }

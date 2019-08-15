af75a099b9ce2417fdbffa45b4d13118befcfbbe
Starz0r <starz0r@starz0r.com>
2019-08-15 00:43:17 -0500

    Reactively change Quassel DNS A Record's IP Value

--- networking.tf
+++ networking.tf
@@ -5,7 +5,11 @@ resource "cloudflare_zone" "starz0r" {
 resource "cloudflare_record" "quassel" {
   domain = "${cloudflare_zone.starz0r.zone}"
   name   = "quassel"
-  value  = "${kubernetes_service.quassel.load_balancer_ingress.0.ip}"
+  value  = "${kubernetes_service.freeman-loadbalancer.load_balancer_ingress.0.ip}"
   type   = "A"
   ttl    = 3600
+}
+
+resource "cloudflare_zone" "iwannacommunity" {
+  zone = "iwannacommunity.com"
 }

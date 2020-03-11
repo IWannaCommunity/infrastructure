4f9c1fedff6ffa94b26de3e3bab5368d93656ac3
Starz0r <starz0r@starz0r.com>
2020-03-11 11:33:00 -0500

    Explicitly use PostgreSQL provider version 1.1.0

--- databases/pg/cluster.tf
+++ databases/pg/cluster.tf
@@ -8,6 +8,7 @@ resource "digitalocean_database_cluster" "pg_master" {
 }
 
 provider "postgresql" {
+  version = "~> 1.1.0"
   alias            = "master"
   host             = "${digitalocean_database_cluster.pg_master.host}"
   port             = "${digitalocean_database_cluster.pg_master.port}"

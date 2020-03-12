0a3d8d1f12e3483813162e6ddf96e9647350bf4b
Starz0r <starz0r@starz0r.com>
2020-03-12 18:14:00 -0500

    Update Freeman K8s cluster to 1.16.6-do.0

--- clusters/freeman/cluster.tf
+++ clusters/freeman/cluster.tf
@@ -1,7 +1,7 @@
 resource "digitalocean_kubernetes_cluster" "freeman" {
   name    = "us-services-freeman"
   region  = "sfo2"
-  version = "1.14.10-do.0"
+  version = "1.16.6-do.0"
   tags    = ["production"]
 
   node_pool {

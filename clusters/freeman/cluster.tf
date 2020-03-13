30f50a86144b02f444568b17b7b20bf12c89eb54
Starz0r <starz0r@starz0r.com>
2020-03-12 20:02:55 -0500

    Method of authenticating on the DO K8s has changed

--- clusters/freeman/cluster.tf
+++ clusters/freeman/cluster.tf
@@ -15,9 +15,11 @@ provider "kubernetes" {
   version = "~> 1.11.1"
   host    = "${digitalocean_kubernetes_cluster.freeman.endpoint}"
 
-  client_certificate     = "${base64decode(digitalocean_kubernetes_cluster.freeman.kube_config.0.client_certificate)}"
-  client_key             = "${base64decode(digitalocean_kubernetes_cluster.freeman.kube_config.0.client_key)}"
-  cluster_ca_certificate = "${base64decode(digitalocean_kubernetes_cluster.freeman.kube_config.0.cluster_ca_certificate)}"
+  token = "${digitalocean_kubernetes_cluster.freeman.kube_config.0.token}"
+  cluster_ca_certificate = "${base64decode(
+    digitalocean_kubernetes_cluster.freeman.kube_config.0.cluster_ca_certificate
+    )
+  }"
 
   load_config_file = false
 }
